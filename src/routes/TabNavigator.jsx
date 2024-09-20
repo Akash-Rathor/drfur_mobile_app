2f08aa7d47b80f30c4c9bc0300069b964a5ed0be
Akash Rathor <akashrathor096@gmail.com>
2024-09-20 15:44:22 +0530

    minor changes

--- src/routes/TabNavigator.jsx
+++ src/routes/TabNavigator.jsx
@@ -1,58 +1,64 @@
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import React from "react";
-import { Text, TouchableOpacity } from "react-native";
+import {Text, TouchableOpacity, View} from "react-native";
 import Home from "../pages/Home";
 import Profile from "../pages/Profile";
-import Icon, { Icons } from "../utilities/Icons";
+import Icon, {Icons} from "../utilities/Icons";
 
 const Tab = createBottomTabNavigator();
 
 const Tabs = [
-  {
-    route: "Home",
-    label: "Home",
-    inActiveIconName: "home",
-    activeIconName: "home",
-    type: Icons.Entypo,
-    component: Home,
-  },
-  {
-    route: "Profile",
-    label: "Profile",
-    inActiveIconName: "user",
-    activeIconName: "user",
-    type: Icons.FontAwesome,
-    component: Profile,
-  },
+	{
+		route: "Home",
+		label: "Home",
+		inActiveIconName: "home",
+		activeIconName: "home",
+		type: Icons.Entypo,
+		component: Home,
+	},
+	{
+		route: "Profile",
+		label: "Profile",
+		inActiveIconName: "user",
+		activeIconName: "user",
+		type: Icons.FontAwesome,
+		component: Profile,
+	},
 ];
 
 const TabButton = (props) => {
-  const { item, onPress, accessibilityState } = props;
-  const focused = accessibilityState.selected;
+	const {item, onPress, accessibilityState} = props;
+	const focused = accessibilityState.selected;
 
-  return (
+	return (
 		<TouchableOpacity
-			className={`flex mr-2 h-2 justify-center ${focused ? "border-t-2 border-black" : "border-t-2 border-transparent"}`}
+			className="flex justify-center items-center"
 			onPress={onPress}
 			activeOpacity={1}
-			style={[
-				{
-					width: 60,
-					height: 60,
-					alignItems: "center",
-					position: "relative",
-				},
-			]}
+			style={{
+				width: 80,
+				height: 56,
+				alignItems: "center",
+				position: "relative",
+			}}
 		>
+			{/* Line above the icon with rounded bottom */}
+			<View
+				className={`w-12 h-1.5 mb-2.5 ${focused ? "bg-firstPrimary border-b rounded-b-full" : "bg-transparent"}`}
+			/>
+
+			{/* Icon */}
 			<Icon
 				name={focused ? item.activeIconName : item.inActiveIconName}
 				type={item.type}
-				color={focused ? "black" : "#99999690"}
+				color={focused ? "#03346E" : "#99999690"}
 				size={24}
-				className="mt-2"
+				className="" // Adjust margin to add spacing between icon and text
 			/>
+
+			{/* Label */}
 			<Text
-				className={`${focused ? "text-black font-semibold" : "text-[#99999690]"} mb-5`}
+				className={`text-xs ${focused ? "text-firstPrimary font-semibold" : "text-[#99999690]"}`}
 			>
 				{item.label}
 			</Text>
@@ -61,31 +67,30 @@ const TabButton = (props) => {
 };
 
 export default function TabNavigator() {
-  return (
-    <Tab.Navigator
-          screenOptions={{
-            headerShown:false,
-        tabBarStyle: {
-          height: 50,
-          position: "absolute",
-          justifyContent: "center",
-          alignItems: "center",
-          zIndex: 1000,
-          display: "flex",
-        },
-      }}
-    >
-      {Tabs.map((item, index) => (
-        <Tab.Screen
-          key={index}
-          name={item.route}
-          component={item.component}
-          options={{
-            tabBarShowLabel: false,
-            tabBarButton: (props) => <TabButton {...props} item={item} />,
-          }}
-        />
-      ))}
-    </Tab.Navigator>
-  );
+	return (
+		<Tab.Navigator
+			screenOptions={{
+				headerShown: false,
+				tabBarStyle: {
+					height: 60, // Increased height for the tab bar to fit everything
+					position: "absolute",
+					justifyContent: "center",
+					alignItems: "center",
+					zIndex: 1000,
+				},
+			}}
+		>
+			{Tabs.map((item, index) => (
+				<Tab.Screen
+					key={index}
+					name={item.route}
+					component={item.component}
+					options={{
+						tabBarShowLabel: false,
+						tabBarButton: (props) => <TabButton {...props} item={item} />,
+					}}
+				/>
+			))}
+		</Tab.Navigator>
+	);
 }
